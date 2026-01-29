0152b147d59afd976d6757e18210a5fc0b4d99af
umer <syedumerahmedcode@gmail.com>
2026-01-29 18:37:39 +0100

    Feature: Implemented the solution and tested it againsta valid test case scenario.

--- src/App.java
+++ src/App.java
@@ -1,31 +1,41 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");/**
-        * Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+                                            * Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+                                            
+                                            An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+                                            
+                                            
+                                            
+                                            Example 1:
+                                            
+                                            Input: grid = [
+                                            ["1","1","1","1","0"],
+                                            ["1","1","0","1","0"],
+                                            ["1","1","0","0","0"],
+                                            ["0","0","0","0","0"]
+                                            ]
+                                            Output: 1
+                                            Example 2:
+                                            
+                                            Input: grid = [
+                                            ["1","1","0","0","0"],
+                                            ["1","1","0","0","0"],
+                                            ["0","0","1","0","0"],
+                                            ["0","0","0","1","1"]
+                                            ]
+                                            Output: 3
+                                            
+                                            */
+        char[][] grid = {
+            {'1', '0', '0', '1'},
+            {'0', '0', '0', '0'},
+            {'1', '0', '1', '1'}
+        };
 
-An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+        Solution solution = new Solution();
+        int result = solution.numIslands(grid);
+        System.out.println("For the grid given above, the number of islands found are: "+result);
 
- 
-
-Example 1:
-
-Input: grid = [
-  ["1","1","1","1","0"],
-  ["1","1","0","1","0"],
-  ["1","1","0","0","0"],
-  ["0","0","0","0","0"]
-]
-Output: 1
-Example 2:
-
-Input: grid = [
-  ["1","1","0","0","0"],
-  ["1","1","0","0","0"],
-  ["0","0","1","0","0"],
-  ["0","0","0","1","1"]
-]
-Output: 3
- 
-         */
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,51 @@
 public class Solution {
-     public int numIslands(char[][] grid) {
+    public int numIslands(char[][] grid) {
+        /**
+         * get the current row and column values
+         */
+        int row = grid.length;
+        int column = grid[0].length;
+        int island = 0;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                if (grid[i][j] == '1') {
+                    island++;
+                    dfs(i, j, grid);
+                }
+            }
+        }
+        return island;
+
+    }
+
+    private void dfs(int row, int column, char[][] grid) {
+         int newRow = grid.length;
+         int newColumn = grid[0].length;
+        
+         int[][] directions=new int[][]{
+            {0,1},
+            {1,0},
+            {0,-1},
+            {-1,0}                    
+         };
+         /**
+          * check if the rows and columns are out of bound or not?
+          * The current value we are at, inside the grid, if that is already ZERO
+         
+          *  */
+         if (row < 0 || column < 0 || row >= newRow || column >= newColumn || grid[row][column] == '0') {
+             //terminating case
+             return;
+
+         }
+         /**
+          * Update the cell so that it does not mess with subsequent calculations
+          */
+         grid[row][column] = '0';
+         for (int[] dir : directions) {
+            dfs(row+dir[0], column+dir[1], grid);
+        }
         
     }
 
